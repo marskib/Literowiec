067efbc4b09940608d7a88f082cbfb74ffdb95ed
marskib <tata.marcina@autyzmsoft.pl>
2017-10-15 16:46:41 +0200

    niedziela

--- app/src/main/java/autyzmsoft/pl/literowiec/MainActivity.java
+++ app/src/main/java/autyzmsoft/pl/literowiec/MainActivity.java
@@ -12,14 +12,12 @@ import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
-import static autyzmsoft.pl.literowiec.R.id.tvInfo;
-import static autyzmsoft.pl.literowiec.R.id.tvInfo1;
 
 public class MainActivity extends Activity {
 
@@ -39,6 +37,8 @@ public class MainActivity extends Activity {
 
     private RelativeLayout.LayoutParams lParams, layoutParams;
 
+    private Button bZnowu;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +46,7 @@ public class MainActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
         rootLayout = (ViewGroup) findViewById(R.id.view_root);
         img = (ImageView) rootLayout.findViewById(R.id.imageView);
 
@@ -86,6 +87,8 @@ public class MainActivity extends Activity {
         lParams      =  (RelativeLayout.LayoutParams) L01.getLayoutParams();
         layoutParams =  (RelativeLayout.LayoutParams) L01.getLayoutParams();
 
+        bZnowu = (Button) findViewById(R.id.bZnowu);
+
         dostosujDoUrzadzen();
 
     }
@@ -101,51 +104,70 @@ public class MainActivity extends Activity {
         width = displaymetrics.widthPixels;
         height= displaymetrics.heightPixels;
 
-        //Obrazek:
+        //Obrazek - ustawiam w lewym górnym rogu:
         lPar = (RelativeLayout.LayoutParams) img.getLayoutParams();
         lPar.width=width/3;
         lPar.height=height/2;
         lPar.topMargin  = 5;
         lPar.leftMargin = 10;
         img.setLayoutParams(lPar);
+        //Ustawienie etykiet w 'ladnym' kwadracie 4x3:
+        ustawLadnieEtykiety();
+    } //koniec Metody()
 
-        /* Ustawiam Literki/Etykiety L0n wzgledem obrazka i wzgledem siebie - na lewo od obrazka: */
+    private void ustawLadnieEtykiety() {
+    /* *************************************************************************************************** */
+    /* Ustawiam Literki/Etykiety L0n wzgledem obrazka i wzgledem siebie - na lewo od obrazka               */
+    /* Kazdy rząd (3 rzedy) ustawiam niejako osobno, poczynajac od 1-go elementu w rzedzie jako od wzorca. */
+    /* *************************************************************************************************** */
 
+        RelativeLayout.LayoutParams lPar;
         //L01 (1-szy rząd):
         lPar = (RelativeLayout.LayoutParams) L01.getLayoutParams();
-        lPar.leftMargin = ((RelativeLayout.LayoutParams) img.getLayoutParams()).leftMargin + img.getLayoutParams().width;
+        lPar.leftMargin = ((RelativeLayout.LayoutParams) img.getLayoutParams()).leftMargin + img.getLayoutParams().width + 20;
+        lPar.topMargin = ((RelativeLayout.LayoutParams) img.getLayoutParams()).topMargin;
+
+        int marginesTop = (int) getResources().getDimension(R.dimen.margin_top_size_1st_row);
+        lPar.topMargin = marginesTop;
+
         L01.setLayoutParams(lPar);
 
         //L02:  //dalej trzeba uzywac Runnable - czekanie az obiekt L01 'usadowi' sie - inaczej wartosci nieustalobe, czyli ok. 0....
         L01.post(new Runnable() {
             @Override
-            public void run() {
+            public void run() { //czekanie aż policzy sie L01
                 final int width1;
                 width1 = L01.getWidth();
                 RelativeLayout.LayoutParams lParX = (RelativeLayout.LayoutParams) L02.getLayoutParams();
-                lParX.leftMargin = ((RelativeLayout.LayoutParams) L01.getLayoutParams()).leftMargin + 2*width1;
+                lParX.leftMargin = ((RelativeLayout.LayoutParams) L01.getLayoutParams()).leftMargin + (int)(1.1*width1);
+                int marginesTop = (int) getResources().getDimension(R.dimen.margin_top_size_1st_row);
+                lParX.topMargin = marginesTop;
             }
         });
 
         //L03:
         L02.post(new Runnable() {
             @Override
-            public void run() {
+            public void run() { //czekanie aż policzy się L02
                 final int width1;
                 width1 = L02.getWidth();
                 RelativeLayout.LayoutParams lParX = (RelativeLayout.LayoutParams) L03.getLayoutParams();
-                lParX.leftMargin = ((RelativeLayout.LayoutParams) L02.getLayoutParams()).leftMargin + 2*width1;
+                lParX.leftMargin = ((RelativeLayout.LayoutParams) L02.getLayoutParams()).leftMargin + (int)(1.1*width1);
+                int marginesTop = (int) getResources().getDimension(R.dimen.margin_top_size_1st_row);
+                lParX.topMargin = marginesTop;
             }
         });
 
         //L04:
         L03.post(new Runnable() {
             @Override
-            public void run() {
+            public void run() { //czekanie aż policzy się L03
                 final int width1;
                 width1 = L03.getWidth();
                 RelativeLayout.LayoutParams lParX = (RelativeLayout.LayoutParams) L04.getLayoutParams();
-                lParX.leftMargin = ((RelativeLayout.LayoutParams) L03.getLayoutParams()).leftMargin + 2*width1;
+                lParX.leftMargin = ((RelativeLayout.LayoutParams) L03.getLayoutParams()).leftMargin + (int)(1.1*width1);
+                int marginesTop = (int) getResources().getDimension(R.dimen.margin_top_size_1st_row);
+                lParX.topMargin = marginesTop;
             }
         });
 
@@ -153,6 +175,8 @@ public class MainActivity extends Activity {
         //L05: (2-gi rząd):
         lPar = (RelativeLayout.LayoutParams) L05.getLayoutParams();
         lPar.leftMargin = ((RelativeLayout.LayoutParams) img.getLayoutParams()).leftMargin + img.getLayoutParams().width;
+        marginesTop = (int) getResources().getDimension(R.dimen.margin_top_size_2nd_row);
+        lPar.topMargin = marginesTop;
         L05.setLayoutParams(lPar);
 
         //L06
@@ -163,6 +187,8 @@ public class MainActivity extends Activity {
                 width1 = L05.getWidth();
                 RelativeLayout.LayoutParams lParX = (RelativeLayout.LayoutParams) L06.getLayoutParams();
                 lParX.leftMargin = ((RelativeLayout.LayoutParams) L05.getLayoutParams()).leftMargin + (int) (1.5*width1);
+                int marginesTop = (int) getResources().getDimension(R.dimen.margin_top_size_2nd_row);
+                lParX.topMargin = marginesTop;
             }
         });
 
@@ -174,6 +200,8 @@ public class MainActivity extends Activity {
                 width1 = L06.getWidth();
                 RelativeLayout.LayoutParams lParX = (RelativeLayout.LayoutParams) L07.getLayoutParams();
                 lParX.leftMargin = ((RelativeLayout.LayoutParams) L06.getLayoutParams()).leftMargin + (int)(1.5*width1);
+                int marginesTop = (int) getResources().getDimension(R.dimen.margin_top_size_2nd_row);
+                lParX.topMargin = marginesTop;
             }
         });
 
@@ -185,23 +213,29 @@ public class MainActivity extends Activity {
                 width1 = L07.getWidth();
                 RelativeLayout.LayoutParams lParX = (RelativeLayout.LayoutParams) L08.getLayoutParams();
                 lParX.leftMargin = ((RelativeLayout.LayoutParams) L07.getLayoutParams()).leftMargin + (int)(1.5*width1);
+                int marginesTop = (int) getResources().getDimension(R.dimen.margin_top_size_2nd_row);
+                lParX.topMargin = marginesTop;
             }
         });
 
 
         //L09: (3-ci rząd):
         lPar = (RelativeLayout.LayoutParams) L09.getLayoutParams();
-        lPar.leftMargin = ((RelativeLayout.LayoutParams) img.getLayoutParams()).leftMargin + img.getLayoutParams().width;
+        lPar.leftMargin = ((RelativeLayout.LayoutParams) img.getLayoutParams()).leftMargin + img.getLayoutParams().width + 40;
+        marginesTop = (int) getResources().getDimension(R.dimen.margin_top_size_3rd_row);
+        lPar.topMargin = marginesTop;
         L09.setLayoutParams(lPar);
 
-        //L10
+        //L10:
         L09.post(new Runnable() {
             @Override
             public void run() {
                 final int width1;
                 width1 = L09.getWidth();
                 RelativeLayout.LayoutParams lParX = (RelativeLayout.LayoutParams) L10.getLayoutParams();
-                lParX.leftMargin = ((RelativeLayout.LayoutParams) L09.getLayoutParams()).leftMargin + 2*width1;
+                lParX.leftMargin = ((RelativeLayout.LayoutParams) L09.getLayoutParams()).leftMargin + (int)(1.3*width1);
+                int marginesTop = (int) getResources().getDimension(R.dimen.margin_top_size_3rd_row);
+                lParX.topMargin = marginesTop;
             }
         });
 
@@ -212,18 +246,9 @@ public class MainActivity extends Activity {
                 final int width1;
                 width1 = L10.getWidth();
                 RelativeLayout.LayoutParams lParX = (RelativeLayout.LayoutParams) L11.getLayoutParams();
-                lParX.leftMargin = ((RelativeLayout.LayoutParams) L10.getLayoutParams()).leftMargin + 2*width1;
-            }
-        });
-
-        //L11:
-        L10.post(new Runnable() {
-            @Override
-            public void run() {
-                final int width1;
-                width1 = L10.getWidth();
-                RelativeLayout.LayoutParams lParX = (RelativeLayout.LayoutParams) L11.getLayoutParams();
-                lParX.leftMargin = ((RelativeLayout.LayoutParams) L10.getLayoutParams()).leftMargin + 2*width1;
+                lParX.leftMargin = ((RelativeLayout.LayoutParams) L10.getLayoutParams()).leftMargin + (int)(1.3*width1);
+                int marginesTop = (int) getResources().getDimension(R.dimen.margin_top_size_3rd_row);
+                lParX.topMargin = marginesTop;
             }
         });
 
@@ -232,21 +257,22 @@ public class MainActivity extends Activity {
             @Override
             public void run() {
                 final int width1;
-                width1 = L11.getWidth();
+                width1 = L10.getWidth();
                 RelativeLayout.LayoutParams lParX = (RelativeLayout.LayoutParams) L12.getLayoutParams();
-                lParX.leftMargin = ((RelativeLayout.LayoutParams) L11.getLayoutParams()).leftMargin + 2*width1;
+                lParX.leftMargin = ((RelativeLayout.LayoutParams) L11.getLayoutParams()).leftMargin + (int)(1.3*width1);
+                int marginesTop = (int) getResources().getDimension(R.dimen.margin_top_size_3rd_row);
+                lParX.topMargin = marginesTop;
             }
         });
 
-//        //dodatkowe rozsunięcie wzgledem siebie:
+        // dodatkowe rozsunięcie wzgledem siebie:
 //        TextView[] lbs = {L01,L02,L03,L04};
 //        for (int i = 1; i < lbs.length; i++) {
 //            lPar = (RelativeLayout.LayoutParams) lbs[i].getLayoutParams();
 //            lPar.leftMargin = 500;//lPar.leftMargin + 250;//*i;
 //            lbs[i].setLayoutParams(lPar);
 //        }
-
-    } //koniec Metody()
+    }  //koniec Metody()
 
 
     private final class ChoiceTouchListener implements OnTouchListener {
@@ -275,14 +301,20 @@ public class MainActivity extends Activity {
                     tvInfo.setText("xKontrolki="+Integer.toString(layoutParams.leftMargin));
                     tvInfo1.setText("xPalca="+Integer.toString(Xstop));
                     break;
+                /*
                 case MotionEvent.ACTION_POINTER_DOWN:
                     break;
                 case MotionEvent.ACTION_POINTER_UP:
                     break;
+                 */
             }
             rootLayout.invalidate();
             return true;
         }
+    } //koniec Metody()
+
+    public void bZnowuOnClick(View v) {
+        ustawLadnieEtykiety();
     }
 
 
